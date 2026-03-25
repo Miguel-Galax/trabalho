cd589dafa9b49eef17b4904298217af36d336baf
Miguel-Galax <miguel.marques.santos@escola.pr.gov.br>
2026-03-25 12:02:28 -0300

    teste2

--- trabalho.py
+++ trabalho.py
@@ -1,3 +1,19 @@
-x=10
-y=20
-print=(x+y)
+# 1. Soma de dois números
+n1 = int(input("Digite o primeiro número: "))
+n2 = int(input("Digite o segundo número: "))
+print(f"A soma é: {n1 + n2}")
+
+# 2. Subtração de variáveis fixas
+numero1 = 20
+numero2 = 8
+print(f"A subtração de {numero1} - {numero2} é: {numero1 - numero2}")
+
+# 3. Dobro de um número
+num = int(input("Digite um número: "))
+print(f"O dobro de {num} é: {num * 2}")
+
+num1= int (input("Digite um número"))
+num2= int (input("Digite um número"))
+print=(f"A some é: {num1+num2}")
+print=(f"A subtração de {num1} - {num2} é: {num1 - num2}")
+print=(f"O dobro de {num1} * {num2} é: {num1 * num2}")
