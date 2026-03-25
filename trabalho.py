9d2184e768d15c5d58bc61934c3072343c9ac01e
Miguel-Galax <miguel.marques.santos@escola.pr.gov.br>
2026-03-25 12:20:34 -0300

    teste4

--- trabalho.py
+++ trabalho.py
@@ -1,30 +1,49 @@
 # 1. Soma de dois números
 n1 = int(input("Digite o primeiro número: "))
 n2 = int(input("Digite o segundo número: "))
-print(f"A soma é: {n1 + n2}")
+print=(f"A soma é: {n1 + n2}")
 
 # 2. Subtração de variáveis fixas
 numero1 = 20
 numero2 = 8
-print(f"A subtração de {numero1} - {numero2} é: {numero1 - numero2}")
+print=(f"A subtração de {numero1} - {numero2} é: {numero1 - numero2}")
 
 # 3. Dobro de um número
 num = int(input("Digite um número: "))
-print(f"O dobro de {num} é: {num * 2}")
+print=(f"O dobro de {num} é: {num * 2}")
 
 # 4. 
 num1= int (input("Digite um número"))
 num2= int (input("Digite um número"))
-print=(f"A some é: {num1+num2}  A subtração é: {num1 - num2} O dobro é: {num1 *num2}" )
+print=(f"A some é: {num1+num2}  A subtração é: {num1 - num2} O dobro é: {num1 * num2}" )
 
 # 5.
 preço=5
 quantidade=3
 total= preço * quantidade
-print(f"O valor total da compra R$: {total:.2f} ")
+print=(f"O valor total da compra R$: {total:.2f} ")
 
 #6.
 idade= int(input("Qual sua idade atual"))
-print(f"A sua idade daqui a dez anos será {idade + 10}")
+print=(f"A sua idade daqui a dez anos será {idade + 10}")
 
 #7.
+num3 = int(input("Digite o primeiro valor: "))
+num4 = int(input("Digite o segundo valor: "))
+print=(f"A diferença entre eles é: {num3 - num4}")
+
+#8.
+num5 = int(input("Digite um número: "))
+print=(f"O triplo é:{num5 * 3} O quádruplo desse número é:{num5 * 4}")
+
+#9.
+num6= int (input("Digite um número"))
+num7= int (input("Digite um número"))
+num8= int (input("Digite um número"))
+print(f"A some de tudo da igual a:{num6 + num7 + num8} ")
+
+#10.
+salario= 1500
+aumento=300
+novo_salario= salario + aumento
+print(f"O novo salario será: {novo_salario}")
